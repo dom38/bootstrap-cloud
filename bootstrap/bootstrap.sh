61e9877b5f0a43d0525552b37a0f8f6756824bbc
Dominic Byrne <dominicbyrne38@hotmail.com>
2024-06-22 15:24:27 +0100

    Update bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -2,3 +2,5 @@
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f bootstrap/core-project.yaml
+kubectl apply -f bootstrap/core-application.yaml
