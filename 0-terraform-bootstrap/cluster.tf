ebd4440f689d044f4f5f008598eceaf8fe4f706a
Dominic Byrne <dominicbyrne38@hotmail.com>
2024-07-02 10:32:57 +0100

    Remove cluster deletion protection

--- 0-terraform-bootstrap/cluster.tf
+++ 0-terraform-bootstrap/cluster.tf
@@ -25,6 +25,8 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
+  deletion_protection = false
+
   workload_identity_config {
     workload_pool = "${var.project_id}.svc.id.goog"
   }
